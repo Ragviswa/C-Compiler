9b5a0bd2c71a00f3dc83a27641d3ab917f4e9c8d
Ragviswa <rv519@ic.ac.uk>
2021-03-14 21:30:32 +0000

    adding CompileRec to unary operators

--- include/ast/ast_unary.hpp
+++ include/ast/ast_unary.hpp
@@ -45,6 +45,12 @@ public:
     virtual const char *getOpcode() const override
     { return "-"; }
 
+    virtual void CompileRec(std::string destReg) const override {
+        std::string srcRegA = makeName("srcRegA");
+        getExpr()->CompileRec(srcRegA);
+        std::cout << "subu " << destReg << " $0 " << srcRegA << std::endl;
+    }
+
     virtual double evaluate(
         const std::map<std::string, double> &bindings
     ) const override
@@ -64,6 +70,16 @@ public:
 
     virtual const char *getOpcode() const override
     { return "!"; }
+    
+    virtual void CompileRec(std::string destReg) const override {
+        std::string srcRegA = makeName("srcRegA");
+        getExpr()->CompileRec(srcRegA);
+        std::string set_zero = makeName("set_zero");
+        std::cout << "beq " << srcRegA << " $0 " << set_zero << std::endl;
+        std::cout << "addi " << destReg << " $0 1" << std::endl;
+        std::cout << ":" << set_zero << std::endl;
+        std::cout << "add " << destReg << " $0 $0" << std::endl;
+    }
 
     virtual double evaluate(
         const std::map<std::string, double> &bindings
@@ -71,26 +87,6 @@ public:
     {
         double in=getExpr()->evaluate(bindings);
         return (!in);
-    }
-};
-
-class Return
-    : public Unary
-{
-public:
-    Return(const ExpressionPtr _expr)
-        : Unary(_expr)
-    {}
-
-    virtual const char *getOpcode() const override
-    { return "return"; }
-
-    virtual double evaluate(
-        const std::map<std::string, double> &bindings
-    ) const override
-    {
-        double in=getExpr()->evaluate(bindings);
-        return in;
     }
 };
 
