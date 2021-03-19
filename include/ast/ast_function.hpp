af6213b3f16150a68a005afc7b124c6ee2ac2b28
HyunjoonJeon <hj1119@ic.ac.uk>
2021-03-19 19:04:37 +0000

    fixed function CompileRec

--- include/ast/ast_function.hpp
+++ include/ast/ast_function.hpp
@@ -41,7 +41,32 @@ public:
 
     void CompileRec(std::string destReg) const {
         std::cout << ":" << name->getId() << std::endl;
+        std::cout << "addiu $sp, $sp, -44" << std::endl;
+        std::cout << "sw $s0, 4($sp)" << std::endl;
+        std::cout << "sw $s1, 8($sp)" << std::endl;
+        std::cout << "sw $s2, 12($sp)" << std::endl;
+        std::cout << "sw $s3, 16($sp)" << std::endl;
+        std::cout << "sw $s4, 20($sp)" << std::endl;
+        std::cout << "sw $s5, 24($sp)" << std::endl;
+        std::cout << "sw $s6, 28($sp)" << std::endl;
+        std::cout << "sw $s7, 32($sp)" << std::endl;
+        std::cout << "sw $fp, 36($sp)" << std::endl;
+        std::cout << "sw $ra, 40($sp)" << std::endl;
+
         statements->CompileRec(destReg);
+
+        std::cout << "lw $s0, 4($sp)" << std::endl;
+        std::cout << "lw $s1, 8($sp)" << std::endl;
+        std::cout << "lw $s2, 12($sp)" << std::endl;
+        std::cout << "lw $s3, 16($sp)" << std::endl;
+        std::cout << "lw $s4, 20($sp)" << std::endl;
+        std::cout << "lw $s5, 24($sp)" << std::endl;
+        std::cout << "lw $s6, 28($sp)" << std::endl;
+        std::cout << "lw $s7, 32($sp)" << std::endl;
+        std::cout << "lw $fp, 36($sp)" << std::endl;
+        std::cout << "lw $ra, 40($sp)" << std::endl;
+        std::cout << "move $fp, $sp" << std::endl;
+        std::cout << "jr $ra" << std::endl;
     }
 
     void print(std::ostream &dst) const
